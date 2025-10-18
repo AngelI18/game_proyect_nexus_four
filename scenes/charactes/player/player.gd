289e8f96a2a5a4a945d58fd2e207c0e3aee03698
AngelI18 <angelcastilloiriarte13@gmail.com>
2025-10-17 22:10:15 -0300

    test level y modificaciones de assets y cambio de scrip simple

--- scenes/charactes/player/player.gd
+++ scenes/charactes/player/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-const SPEED = 120.0
+# Velocidad ajustada para tiles de 24x24 (5 tiles por segundo)
+const SPEED = 170.0
 const JUMP_VELOCITY = -400.0
 
 func _physics_process(delta: float) -> void:
@@ -9,22 +10,30 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Salto
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Movimiento horizontal
+	# Movimiento horizontal sin inercia
 	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction != 0:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED * delta)
+	velocity.x = direction * SPEED
 
 	move_and_slide()
 	_update_animation(direction)
 
 func _update_animation(direction: float) -> void:
-	if direction == 0:
+	if not is_on_floor():
+		# Animación de salto
+		$AnimatedSprite2D.play("jump")
+	elif direction == 0:
+		# Animación de reposo
 		$AnimatedSprite2D.play("idle")
 	else:
+		# Animación de correr
 		$AnimatedSprite2D.play("run")
 		$AnimatedSprite2D.flip_h = direction < 0
+
+
+func _update_collision(animation_name: String) -> void:
+		$CollisionShape2D_idle.set_disabled(animation_name != "idle")
+		$CollisionShape2D_run.set_disabled(animation_name != "run")
+		$CollisionShape2D_jump.set_disabled(animation_name != "jump")
