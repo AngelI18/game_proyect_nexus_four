2b2366e0454bc12538cd0c94c7fa0bb7dc09cdb3
Nicolás Rojas Bustos <nicolas.rojas13@hotmail.com>
2025-10-20 23:03:29 -0300

    add double jump

--- scenes/charactes/player/player.gd
+++ scenes/charactes/player/player.gd
@@ -4,18 +4,14 @@ extends CharacterBody2D
 const SPEED = 170.0
 const JUMP_VELOCITY = -400.0
 
-<<<<<<< HEAD
 var can_double_jump = true
 var cant_double_jump = 2
 var jump_less = cant_double_jump
 
-=======
->>>>>>> 76087abd20f8a246a9036651742304d3d553caa9
 func _physics_process(delta: float) -> void:
 	# Gravedad
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-<<<<<<< HEAD
 	if is_on_floor():
 		can_double_jump = true
 		jump_less = cant_double_jump
@@ -30,12 +26,6 @@ func _physics_process(delta: float) -> void:
 			jump_less -= 1
 		elif jump_less == 0:
 			can_double_jump = false
-=======
-
-	# Salto
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
->>>>>>> 76087abd20f8a246a9036651742304d3d553caa9
 
 	# Movimiento horizontal sin inercia
 	var direction := Input.get_axis("ui_left", "ui_right")
